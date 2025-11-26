edf710cc3fd00049840e7f630b3f4391418f44cf
Jura.me <m@jura.me>
2025-11-26 19:58:48 +0300

    Исправление ошибок lint после 3 этапа ТЗ

--- valutatrade_hub/core/__init__.py
+++ valutatrade_hub/core/__init__.py
@@ -1,5 +1,5 @@
 """Core модуль - модели, утилиты и бизнес-логика."""
 
 from valutatrade_hub.core.models import *
-from valutatrade_hub.core.utils import *
 from valutatrade_hub.core.usecases import *
+from valutatrade_hub.core.utils import *
